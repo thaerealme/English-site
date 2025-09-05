ad175ac820ff72cc4f1191573a071f7d0399018d
Artem Khusnullin <artem@MacBook-Air-Artem.local>
2025-09-05 07:09:21 +0500

    fix: css

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/English-site/', // ОБЯЗАТЕЛЬНО с слэшем в конце
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
+})

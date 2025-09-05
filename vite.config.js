b50347e30f899733e9b907805fac659286e68c7c
Artem Khusnullin <artem@MacBook-Air-Artem.local>
2025-09-05 07:16:37 +0500

    fix: css1

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/English-site/', // ОБЯЗАТЕЛЬНО с слэшем в конце
+  base: '/English-site/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    cssCodeSplit: false, // Убираем CSS из сборки
     rollupOptions: {
       output: {
         manualChunks: undefined,
